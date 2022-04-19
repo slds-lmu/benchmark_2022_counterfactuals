8a06f7378527c69cd00e45caea5ef537c0c63497
susanne-207 <dandl.susanne@gmail.com>
2022-04-19 12:14:57 +0200

    solve bug in problem specification

--- cfactuals/def_cfactuals.R
+++ cfactuals/def_cfactuals.R
@@ -1,8 +1,8 @@
 # DATA configurations
 data_list = readRDS(file.path("data/data_storage/data_list.RDS"))
 data_run_or_walk = data_list$run_or_walk_info
-data_list$run_or_walk_info_sub_1 = list(data = data_run_or_walk[sample(.N, ceiling(nrow(data_run_or_walk) * 0.01))])
-data_list$run_or_walk_info_sub_10 = list(data = data_run_or_walk[sample(.N, ceiling(nrow(data_run_or_walk) * 0.1))])
+data_list$run_or_walk_info_sub_1 = data_run_or_walk[sample(.N, ceiling(nrow(data_run_or_walk) * 0.01))]
+data_list$run_or_walk_info_sub_10 = data_run_or_walk[sample(.N, ceiling(nrow(data_run_or_walk) * 0.1))]
 
 # ades configurations
 models = c("ranger", "xgboost", "svm", "logistic_regression", "neural_network")
