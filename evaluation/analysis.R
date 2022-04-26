e879e806dfab12dcd37e0784c2c7884663e80c46
susanne-207 <dandl.susanne@gmail.com>
2022-04-26 18:06:03 +0200

    evaluation: add analysis on # cfexp

--- evaluation/analysis.R
+++ evaluation/analysis.R
@@ -5,12 +5,25 @@ library(ggplot2)
 library(tidyverse)
 source("evaluation/analysis_helper.R")
 
+p1 = check_cfexp_generated(models = "neural_network")
+# ggsave("evaluation/figures/nn_proportion.pdf", height = 5, width = 8.5, plot = p1)
+
 plot_comparison("diabetis")
 plot_comparison("tic_tac_toe")
 plot_comparison("credit_g")
 plot_comparison("run_or_walk_info")
 plot_comparison("bank8FM")
 plot_comparison("hill_valley")
+
+all_methods = c("moc_icecurve_0", "moc_icecurve_1", "moc_random_0", "moc_random_1", "moc_sd_0", "moc_sd_1",
+               "moc_traindata_0", "moc_traindata_1", "nice" , "whatif")
+
+plot_comparison("diabetis", methods = all_methods)
+plot_comparison("tic_tac_toe", methods = all_methods)
+plot_comparison("credit_g", methods = all_methods)
+plot_comparison("run_or_walk_info", methods = all_methods)
+plot_comparison("bank8FM", methods = all_methods)
+plot_comparison("hill_valley", methods = all_methods)
 
 plot_speed_comparison(type = "n")
 plot_speed_comparison(type = "p")
@@ -24,3 +37,5 @@ psc_all1  = plot_speed_comparison(methods = c("moc_icecurve_0", "moc_icecurve_1"
                                               "moc_traindata_0", "moc_traindata_1", "nice_sparsity", "nice_proximity", "nice_plausibility" , "whatif"), 
                                   savepdf = FALSE, type = "p")
 # ggsave(filename = "evaluation/figures/p_runtime_all.pdf", plot = psc_all1, width = 4, height = 9)
+
+
--- evaluation/analysis_helper.R
+++ evaluation/analysis_helper.R
@@ -1,4 +1,4 @@
-plot_comparison = function(data_set_name, methods = c("whatif", "nice", "moc_icecurve_0", "moc_icecurve_1"), savepdf = FALSE) {
+plot_comparison = function(data_set_name, methods = c("whatif", "nice", "moc_icecurve_0", "moc_icecurve_1"), savepdf = TRUE) {
 
   con = dbConnect(RSQLite::SQLite(), "evaluation/db_evals.db")
   res = tbl(con, paste0(data_set_name, "_EVAL")) %>% collect()
@@ -34,7 +34,7 @@ plot_comparison = function(data_set_name, methods = c("whatif", "nice", "moc_ice
   if (savepdf) {
     fig.path = "evaluation/figures"
     dir.create(fig.path, showWarnings = FALSE)
-    ggsave(filename = file.path(fig.path, paste0(paste(data_set_name, "obj", sep = "_"), ".pdf")), plot = plt, width = 5.5, height = 3.8)
+    ggsave(filename = file.path(fig.path, paste0(paste(data_set_name, "obj_all", sep = "_"), ".pdf")), plot = plt, width = 9, height = 6.5) # 5.5, 3.8
   }
   return(plt)
 }
@@ -113,5 +113,111 @@ plot_speed_comparison = function(type = "n", methods = c("moc_icecurve_0", "moc_
   
 }
 
+check_cfexp_generated = function(data_set_name, models = NULL) {
+  
+  con = dbConnect(RSQLite::SQLite(), "evaluation/db_evals.db")
+  res = list()
+  
+  for (data_name in c("diabetis", "tic_tac_toe", "credit_g", "run_or_walk_info", "hill_valley", "bank8FM")) {
+      if (data_name == "overall") next
+      res_df = tbl(con, paste0(data_name, "_EVAL")) %>% collect() %>% 
+        select(id_x_interest, algo_spec, model_name) %>% 
+        mutate(algo_spec = recode(algo_spec, nice_sparsity = "nice", nice_proximity = "nice", nice_plausibility = "nice")) %>%
+        distinct() %>% 
+        group_by(algo_spec, model_name) %>% 
+        summarise(proportion = n_distinct(id_x_interest)/10) %>% 
+        mutate(data_name = data_name)
+      if (!is.null(models)) {
+        res[[data_name]] = res_df %>% filter(model_name %in% models)
+      } else {
+        res[[data_name]] = res_df
+      }
+  }
+  
+  DBI::dbDisconnect(con)
+  
+  names(res)[names(res) == "diabetis"] = "diabetes"
+  res$diabetes$data_name = "diabetes"
+  names(res)[names(res) == "run_or_walk_info"] = "run_or_walk_information"
+  res$run_or_walk_information$data_name = "run_or_walk_information"
+  
+  if (is.null(models)) {
+    ro = do.call(rbind, res)
+    ro = ro %>% group_by(algo_spec, model_name) %>% summarise(proportion = mean(proportion)) %>% 
+      mutate(data_name = "overall")
+    res[["overall"]] = ro
+    
+    data_set_names = names(res)
+    plt_data = do.call(rbind,res) %>% ungroup()
+    
+    # plt_data = plt_data %>%
+    #   filter(data_name %in% data_set_names) %>%
+    #   mutate(data_name = factor(data_name, levels = data_set_names)) %>% 
+    #   mutate(
+    #     data_name = recode(
+    #       data_name,
+    #       bank8FM = "bank8FM (n = 8,192 | p = 8)",
+    #       credit_g = "credit_g (n = 1,000 | p = 20)",
+    #       diabetes = "diabetes (n = 768 | p = 8)",
+    #       hill_valley = "hill_valley (n = 1,212 | p = 100)",
+    #       run_or_walk_information = "run_or_walk_information (n = 88,588 | p = 6)",
+    #       tic_tac_toe = "tic_tac_toe (n = 958 | p = 9)",
+    #     )
+    #   )
+    p = ggplot(plt_data) +
+      geom_tile(aes(x = model_name, y = algo_spec, fill = proportion), color = "white",
+                lwd = 1.5,
+                linetype = 1) +
+      theme_bw() +
+      scale_fill_gradient(low = "white", high = "blue") +
+      facet_wrap(vars(data_name), ncol = 3) +
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+            panel.background = element_blank(), axis.title.y = element_blank(), axis.title.x = element_blank())
+    
+    p = shift_legend_bottom_right(p)
+    
+} else {
+  
+  ro = do.call(rbind, res)
+  ro = ro %>% group_by(algo_spec, model_name) %>% summarise(proportion = mean(proportion)) %>% 
+    mutate(data_name = "overall")
+  res[["overall"]] = ro
+  
+  plt_data = do.call(rbind,res) %>% ungroup()
+  data_set_names = unique(plt_data$data_name)
+  plt_data = plt_data %>%
+  #   filter(data_name %in% data_set_names) %>%
+  mutate(data_name = factor(data_name, levels = data_set_names)) 
+  #   mutate(
+  #     data_name = recode(
+  #       data_name,
+  #       bank8FM = "bank8FM (n = 8,192 | p = 8)",
+  #       credit_g = "credit_g (n = 1,000 | p = 20)",
+  #       diabetes = "diabetes (n = 768 | p = 8)",
+  #       hill_valley = "hill_valley (n = 1,212 | p = 100)",
+  #       run_or_walk_information = "run_or_walk_information (n = 88,588 | p = 6)",
+  #       tic_tac_toe = "tic_tac_toe (n = 958 | p = 9)",
+  #     )
+  #   )
+  p = ggplot(plt_data) +
+    geom_tile(aes(x = data_name, y = algo_spec, fill = proportion), color = "white",
+              lwd = 1.5,
+              linetype = 1) +
+    theme_bw() +
+    scale_fill_gradient(low = "white", high = "blue") +
+    # facet_wrap(vars(data_name), ncol = 3) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.title.y = element_blank(), axis.title.x = element_blank())
+  } 
+return(p)
+}
 
-
+shift_legend_bottom_right = function(p) {
+  gp = ggplotGrob(p)
+  facet.panels = grep("^panel", gp[["layout"]][["name"]])
+  empty.facet.panels = sapply(facet.panels, function(i) "zeroGrob" %in% class(gp[["grobs"]][[i]]))
+  empty.facet.panels = facet.panels[empty.facet.panels]
+  empty.facet.panels = gp[["layout"]][empty.facet.panels, ]
+  names = empty.facet.panels$name
+  lemon::reposition_legend(p, 'center', panel = names)
+}
