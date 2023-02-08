c8ce5882d4b7527f7a8f36cdfe98412ceb4ba193
susanne-207 <dandl.susanne@gmail.com>
2023-02-08 10:43:31 +0100

    update time tracking

--- cfactuals/helper_cfactuals_wrapper.R
+++ cfactuals/helper_cfactuals_wrapper.R
@@ -7,12 +7,16 @@ whatif_wrapper = function(data, job, instance, ...) {
   pred_x_interest = pred$predict(x_interest)
   desired_class = names(pred_x_interest)[apply(pred_x_interest, 1L, which.min)]
   
+  start_time = Sys.time()
   whatif_classif = WhatIfClassif$new(pred, n_counterfactuals = 10L, distance_function = "gower_c")
   cfactuals = whatif_classif$find_counterfactuals(
     x_interest, desired_class, desired_prob = c(0.5 + sqrt(.Machine$double.eps), 1)
   )
+  end_time = Sys.time()
   
-  get_cf_table(cfactuals, job)
+  # save info on runtime and calls to fhat
+  res = get_cf_table(cfactuals, job)
+  attr(res, "runtime") = as.numeric(end_time - start_time)
 }
 
 # NICE
@@ -24,13 +28,17 @@ nice_wrapper = function(data, job, instance, ...) {
   pred_x_interest = pred$predict(x_interest)
   desired_class = names(pred_x_interest)[apply(pred_x_interest, 1L, which.min)]
   
+  start_time = Sys.time()
   nice_classif = NICEClassif$new(pred, optimization = arg_list$optimization,
                                  return_multiple = TRUE, finish_early = FALSE, distance_function = "gower_c")
   cfactuals = nice_classif$find_counterfactuals(
     x_interest, desired_class, desired_prob = c(0.5 + sqrt(.Machine$double.eps), 1)
   )
+  end_time = Sys.time()
   
-  get_cf_table(cfactuals, job)
+  # save info on runtime and calls to fhat
+  res = get_cf_table(cfactuals, job)
+  attr(res, "runtime") = as.numeric(end_time - start_time)
 }
 
 # MOC
@@ -42,6 +50,7 @@ moc_wrapper = function(data, job, instance, ...) {
   pred_x_interest = pred$predict(x_interest)
   desired_class = names(pred_x_interest)[apply(pred_x_interest, 1L, which.min)]
   
+  start_time = Sys.time()
   moc_classif = MOCClassif$new(
     pred, termination_crit = "genstag", n_generations = 10L, 
     init_strategy = "icecurve", use_conditional_mutator = FALSE,
@@ -51,25 +60,11 @@ moc_wrapper = function(data, job, instance, ...) {
   cfactuals = moc_classif$find_counterfactuals(
     x_interest, desired_class, desired_prob = c(0.5 + sqrt(.Machine$double.eps), 1)
   )
+  end_time = Sys.time()
   
-  get_cf_table(cfactuals, job)
-}
-
-
-# Random Search
-random_search_wrapper = function(data, job, instance, ...) {
-  
-  arg_list = list(...)
-  x_interest = readRDS(file.path("data/data_storage/x_interest_list.RDS"))[[job$prob.name]][arg_list$id_x_interest]
-  pred = get_predictor_and_x_interest_pp(arg_list, job, data)
-  pred_x_interest = pred$predict(x_interest)
-  desired_class = names(pred_x_interest)[apply(pred_x_interest, 1L, which.min)]
-  
-  rs = RandomSearchClassif$new(pred, distance_function = "gower_c")
-  cfactuals = rs$find_counterfactuals(
-    x_interest, desired_class = desired_class, desired_prob = c(0.5 + sqrt(.Machine$double.eps), 1)
-  )
-  get_cf_table(cfactuals, job)
+  # save info on runtime and calls to fhat
+  res = get_cf_table(cfactuals, job)
+  attr(res, "runtime") = as.numeric(end_time - start_time)
 }
 
 # Function to create results table
