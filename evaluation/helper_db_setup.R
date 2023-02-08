14a4b78458babc88b772ef450f9113d769c2c13c
susanne-207 <dandl.susanne@gmail.com>
2023-02-08 10:52:08 +0100

    evaluation: use own time running

--- evaluation/helper_db_setup.R
+++ evaluation/helper_db_setup.R
@@ -9,12 +9,11 @@ add_results_to_db = function(data_set_name, reg) {
     this_job = job_overview[job.id == job_id]
     cfactuals = try(readRDS(file.path(reg_dir, "results", paste0(this_job$job.id, ".rds"))))
     cfactuals$job.id = NULL
-    attr(cfactuals, "time_running") <- getJobStatus(this_job, reg)$time.running
     
     if (inherits(cfactuals, "try-error")) next
     print(job_id)
     if (nrow(cfactuals) > 0L) {
-      dt_standard = cbind(cfactuals, this_job, "time_running" = as.numeric(attr(cfactuals, "time_running")))
+      dt_standard = cbind(cfactuals, this_job, "time_running" = attr(cfactuals, "runtime"))
       if (!toupper(data_set_name) %in% DBI::dbListTables(con)) {
         dbWriteTable(con, toupper(data_set_name), as.data.frame(dt_standard), overwrite = TRUE)
       } else {
