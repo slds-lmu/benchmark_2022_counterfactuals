183bcada5d5855f273e2a16028c96e67befb42b8
susanne-207 <dandl.susanne@gmail.com>
2023-06-05 15:03:52 +0200

    models: make sure mlr3keras is properly installed

--- models/libs_models.R
+++ models/libs_models.R
@@ -1,5 +1,7 @@
-packages = c("batchtools", "mlr3", "mlr3learners", "mlr3pipelines", "mlr3tuning", "mlr3keras", "mlr3keras")
+packages = c("batchtools", "mlr3", "mlr3learners", "mlr3pipelines", "mlr3tuning", "remotes")
 new_packages = packages[!(packages %in% installed.packages()[,"Package"])]
 if (length(new_packages) > 0L) install.packages(new_packages)
+
+if (!"mlr3keras" %in% installed.packages()) remotes::install_github("mlr-org/mlr3keras")
 
 sapply(packages, require, character.only = TRUE)
