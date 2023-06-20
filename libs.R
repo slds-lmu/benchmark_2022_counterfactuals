0bab6e0956bc1fac46d23459a5d775482efc99a6
susanne-207 <dandl.susanne@gmail.com>
2023-06-20 16:49:30 +0200

    add general libs.R file

---
+++ libs.R
@@ -0,0 +1,28 @@
+#----
+# Overview of required packages
+#----
+source("config.R")
+
+# Packages only required when TEST = TRUE
+packages = c(
+  ### data: make get-data
+  "mlr3oml", 
+  ### models: make train-models & make resample
+  "batchtools", "mlr3", "mlr3learners", "mlr3pipelines", "mlr3tuning", 
+  ### counterfactuals: make find-counterfactuals
+  "counterfactuals", "iml", "keras", "data.table"
+)
+
+# Additional packages if TEST = FALSE
+if (!TEST) {
+  packages = c(packages, 
+    ### models: make train-models & make resample
+    "remotes", "xgboost", "ranger", "e1071","mlr3keras"
+  )
+  if (!"mlr3keras" %in% installed.packages()) remotes::install_github("mlr-org/mlr3keras")
+}
+
+# Load packages or install them
+new_packages = packages[!(packages %in% installed.packages()[,"Package"])]
+if (length(new_packages) > 0L) install.packages(new_packages)
+sapply(packages, require, character.only = TRUE)
