a375a39b74be80b6dc907168f26e5d0790446e6c
susanne-207 <dandl.susanne@gmail.com>
2023-06-20 16:50:07 +0200

    update libs.R

--- libs.R
+++ libs.R
@@ -25,4 +25,3 @@ if (!TEST) {
 # Load packages or install them
 new_packages = packages[!(packages %in% installed.packages()[,"Package"])]
 if (length(new_packages) > 0L) install.packages(new_packages)
-sapply(packages, require, character.only = TRUE)
